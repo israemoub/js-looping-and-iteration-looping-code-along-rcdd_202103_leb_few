d5dce89acac0574c1666e85238de791bf50aea9f
Israe Moubayed <israetarek@yahoo.com>
2021-05-28 07:55:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ writeCards(["Ada", "Brendan", "Ali"], "birthday");
 function writeCards(names, event) {
   let arr = [];
   for(let i=0; i<names.length; i++) {
-    arr = ["Thank you, ${names}, for the wonderful ${event} gift!"]
+    arr[i] = `Thank you, ${names}, for the wonderful ${event} gift!`
   }
+  return arr;
 }
